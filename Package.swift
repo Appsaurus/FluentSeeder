3ffc9a00b04ca3f2aacccd3d8e63f2885fbe31a3
Brian Strobach <6342694+Strobocop@users.noreply.github.com>
2021-11-10 16:39:35 -0600

    Dependency updates. Fixed tests.

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.4
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -24,7 +24,7 @@ let package = Package(
         .package(url: "https://github.com/vapor/fluent.git", .upToNextMajor(from:"4.0.0")),
 		.package(url: "https://github.com/vapor/fluent-sqlite-driver.git", .upToNextMajor(from:"4.0.0")),
 		.package(url: "https://github.com/Appsaurus/RandomFactory.git", .branch("swift-5.4")),
-		.package(url: "https://github.com/Appsaurus/FluentExtensions.git", .branch("vapor-4"))
+        .package(url: "https://github.com/Appsaurus/FluentExtensions.git", .upToNextMajor(from:"1.0.0"))
     ],
     targets: [
         .target(
